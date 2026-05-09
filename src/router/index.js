de015f56782b5db7f97eb739369eed9542799a54
darrenori <darrennorii@gmail.com>
2026-05-09 23:32:21 +0800

    Set up CI/CD and GitHub Pages deployment

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import DashboardView from '../views/DashboardView.vue'
 import ProfileView from '../views/ProfileView.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/login', name: 'login', component: LoginView },
         { path: '/', name: 'dashboard', component: DashboardView },
